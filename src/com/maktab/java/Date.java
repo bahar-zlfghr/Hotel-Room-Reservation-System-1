a2ca04906d7d971981bf470369c4588f506d6909
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-03-02 15:45:08 +0330

    Create Date class and implementation it

---
+++ src/com/maktab/java/Date.java
@@ -0,0 +1,34 @@
+package com.maktab.java;
+
+public class Date implements Comparable<Date> {
+    private final int year;
+    private final int month;
+    private final int day;
+
+    public Date(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    @Override
+    public int compareTo(Date date) {
+        if (this.year < date.year) {
+            return 1;
+        }
+        else if (this.year == date.year) {
+            if (this.month < date.month) {
+                return 1;
+            }
+            else if (this.month == date.month) {
+                if (this.day < date.day) {
+                    return 1;
+                }
+                return -1;
+
+            }
+            return -1;
+        }
+        return -1;
+    }
+}
